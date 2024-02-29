3072461667326223bd266d3673194486e2bed833
sameer <sameer.ali@evampsaanga.com>
2024-02-29 11:20:32 +0500

    Quiz and event apis revised

--- src/main/java/com/rtechnologies/soies/repository/QuizRepository.java
+++ src/main/java/com/rtechnologies/soies/repository/QuizRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findByTeacherId(Long teacherId);
     List<Quiz> findByCourseId(Long courseId);
 }
