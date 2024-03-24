9d93c5d9bb57e98d5942f363bf14c0b12f881264
sameer <sameer.ali@evampsaanga.com>
2024-03-24 14:30:21 +0500

    dummy message

--- src/main/java/com/rtechnologies/soies/SoiesApplication.java
+++ src/main/java/com/rtechnologies/soies/SoiesApplication.java
@@ -1,7 +1,11 @@
 package com.rtechnologies.soies;
 
+import com.rtechnologies.soies.model.dto.CreateTeacherDTO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @SpringBootApplication
 public class SoiesApplication {
@@ -9,4 +13,9 @@ public class SoiesApplication {
 		SpringApplication.run(SoiesApplication.class, args);
 	}
 
+	@PostMapping("/message")
+	public ResponseEntity<?> dummy() {
+		return ResponseEntity.status(200)
+				.body("Doneee");
+	}
 }
