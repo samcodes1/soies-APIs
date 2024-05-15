9c6b029e262275a682931b00643341ddbe454d79
sameer <sameer.ali@evampsaanga.com>
2024-05-15 23:53:09 +0500

    Auth API improved

--- src/main/java/com/rtechnologies/soies/service/CustomUserDetailService.java
+++ src/main/java/com/rtechnologies/soies/service/CustomUserDetailService.java
@@ -44,7 +44,7 @@ public class CustomUserDetailService implements UserDetailsService {
                     .teacher(null)
                     .student(null)
                     .admin(admin.get())
-                    .authorities(Collections.singletonList(() -> "ROLE_TEACHER")) // Assuming "ROLE_TEACHER" as the authority for teachers
+                    .authorities(Collections.singletonList(() -> "ROLE_ADMIN")) // Assuming "ROLE_TEACHER" as the authority for teachers
                     .build();
         }
 
