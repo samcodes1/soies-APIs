e6c91c5c73de766e532fbc6ab1798b012753dee9
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-23 00:53:16 +0500

    Delete src/main/java/com/rtechnologies/soies/repository directory
    will be uploading shortly

--- src/main/java/com/rtechnologies/soies/repository/CourseLectureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rtechnologies.soies.repository;
-
-import com.rtechnologies.soies.model.association.CourseLecture;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CourseLectureRepository extends JpaRepository<CourseLecture, Long> {
-}
--- src/main/java/com/rtechnologies/soies/repository/QuizStudentAnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rtechnologies.soies.repository;
-
-import com.rtechnologies.soies.model.association.QuizStudentAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizStudentAnswerRepository extends JpaRepository<QuizStudentAnswer, Long> {
-}
