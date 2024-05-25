53e85afd993f455a3e94fdd3bf44ea752bd06d9d
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-25 17:14:09 +0500

    Delete src/main/java/com/rtechnologies/soies/model directory

--- src/main/java/com/rtechnologies/soies/model/Permission.java
+++
@@ -1,6 +0,0 @@
-package com.rtechnologies.soies.model;
-
-public class Permission {
-    public static final String READ_TEACHER = "READ_TEACHER";
-    public static final String CREATE_TEACHER = "CREATE_TEACHER";
-}
