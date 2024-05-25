11e9e3922449bd7e7c1557b62af1342f5341040a
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-25 16:57:51 +0500

    Delete src/main/java/com/rtechnologies/soies/model directory

--- src/main/java/com/rtechnologies/soies/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.rtechnologies.soies.model;
-
-public class Role {
-    public static final String ROLE_USER = "ROLE_USER";
-    public static final String ROLE_TEACHER = "ROLE_TEACHER";
-}
