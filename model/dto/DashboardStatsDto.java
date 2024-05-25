63f4c66a8dec178b0d3567b40e9d902a77dd9349
alisrepository <49435870+alisrepository@users.noreply.github.com>
2024-05-25 17:21:41 +0500

    Delete model directory

--- model/dto/DashboardStatsDto.java
+++
@@ -1,9 +0,0 @@
-package com.rtechnologies.soies.model.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public interface DashboardStatsDto {
-    Float getPercent();
-    String getGrade();
-}
