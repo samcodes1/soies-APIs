6a0f7e20c0f26945c93a2b4f6fe22f58e30d971c
Osam-ullah <osamullah@rtechnologiespk.com>
2024-09-16 17:38:07 +0500

    fix: fixed deletion by multiple ids

---
+++ src/main/java/com/rtechnologies/soies/model/dto/StudentIdsRequest.java
@@ -0,0 +1,16 @@
+package com.rtechnologies.soies.model.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentIdsRequest {
+    private List<Long> studentIds;
+}
